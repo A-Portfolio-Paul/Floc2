1b72f9d05ed5813fe31176d38ad67e18992d2a4b
PaulAyling <jimmyyukka@gmail.com>
2022-03-22 13:32:09 +0100

    Removed Docs: So I can focus on data

--- src/lib/components/docs/document/preview/PToolbar.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    export let bodyVisible 
-    export let cardId
-    import BodyVisible from '../furniture/buttons/BodyVisible.svelte';
-
-</script>
-<nav class="flex flex-row">
-    <BodyVisible bind:bodyVisible={bodyVisible}/>
-</nav>  
--- src/lib/components/docs/document/preview/Preview.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	export let cardId;
-	export let colorShade;
-	import PreviewCard from './PCard.svelte';
-	
-</script>
-
-<h1>Preview</h1>
-<PreviewCard {cardId}  {colorShade}/>
