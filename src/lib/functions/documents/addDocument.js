f38a4730618fdc56ec22ca97088710c335a0f3c7
PaulAyling <jimmyyukka@gmail.com>
2022-03-30 13:18:00 +0200

    add funcitons

--- src/lib/functions/documents/addDocument.js
+++ src/lib/functions/documents/addDocument.js
@@ -1,16 +1,41 @@
-import {newId} from 'newid'
+// import {newId} from 'newid'
+import {auth_user_mock} from '../../mockupData/authUser'
+import { v4 as uuidv4 } from 'uuid';
+import {documents} from '../../stores'
+import {cuurentView} from '../../stores'
 
 
 
 
 export const addDocument = () =>{
-    console.log('addDoc running.......')
-    const newDoc = {
-        export const document1 = {
-            docId:newId(),
-            createDate:,
-            createdBy:'ur-101',
-            title:'Causes & Treatments for Pancreatitus'
+    documents.update((value) => {
+        const newDoc = {
+            docId:uuidv4(),
+            createDate:new Date(),
+            createdBy:auth_user_mock.userId,
+            title:'New Document'
         }
+        value.push(newDoc)
+        console.log('VALUE',value)
+        return value
+	});
+    cuurentView.update((value) => {
+        const newView = 
+
+
+
+        
+        value.push(newDoc)
+        console.log('VALUE',value)
+        return value
+	});
+
+
+
+
+
+
+
+
     }
-}
+
