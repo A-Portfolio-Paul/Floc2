1769b96b2103336b826e97f9350ee8e07f00982e
PaulAyling <jimmyyukka@gmail.com>
2022-03-30 10:03:31 +0200

    showUrl is working

--- src/lib/components/docs/document/furniture/buttons/Toolbar.svelte
+++ src/lib/components/docs/document/furniture/buttons/Toolbar.svelte
@@ -1,18 +1,18 @@
 <script>
-    import { removeRecord } from '../../../../../functions/documents/remove2';
+    import { removeRecord } from '../../../../../functions/documents/remove';
 
     export let bodyVisible 
     export let editUrl
     export let cardId
     // import BodyVisible from './BodyVisible.svelte';
     import Remove from './Remove.svelte'
-    // import ShowUrl from './EditUrl.svelte';
+    import ShowUrl from './EditUrl.svelte';
     // import Cols from './Cols.svelte'
 </script>
 
 <nav class="flex flex-row">
 	<Remove {cardId} />
-	<!-- <ShowUrl bind:editUrl={editUrl}/> -->
+	<ShowUrl bind:editUrl={editUrl}/>
 	<!-- <Cols bind:cols={cols} /> -->
 	<!-- <BodyVisible bind:bodyVisible={bodyVisible}/> -->
 </nav>
