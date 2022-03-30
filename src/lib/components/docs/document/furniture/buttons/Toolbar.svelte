7747f07fac06e6123941bea31d3847162ecc3a2f
PaulAyling <jimmyyukka@gmail.com>
2022-03-30 10:05:00 +0200

    bodyvisible works

--- src/lib/components/docs/document/furniture/buttons/Toolbar.svelte
+++ src/lib/components/docs/document/furniture/buttons/Toolbar.svelte
@@ -4,15 +4,13 @@
     export let bodyVisible 
     export let editUrl
     export let cardId
-    // import BodyVisible from './BodyVisible.svelte';
+    import BodyVisible from './BodyVisible.svelte';
     import Remove from './Remove.svelte'
     import ShowUrl from './EditUrl.svelte';
-    // import Cols from './Cols.svelte'
 </script>
 
 <nav class="flex flex-row">
 	<Remove {cardId} />
 	<ShowUrl bind:editUrl={editUrl}/>
-	<!-- <Cols bind:cols={cols} /> -->
-	<!-- <BodyVisible bind:bodyVisible={bodyVisible}/> -->
+	<BodyVisible bind:bodyVisible={bodyVisible}/>
 </nav>
